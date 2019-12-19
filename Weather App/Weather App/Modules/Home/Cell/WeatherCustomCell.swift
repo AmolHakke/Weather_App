531f3bb6693c8175bc4a49a596a534d38ca0c2b8
yalamandarao inaganti <yalamandarao.i@gmail.com>
2019-12-19 16:36:08 +0800

    # WeatherHomeViewController - Added Tableview and Weather Custom cell - Displaying dummy data in the UITabelview -  Navigate to WetherDetailViewController page once user click on weather.

---
+++ Weather App/Weather App/Modules/Home/Cell/WeatherCustomCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherCustomCell.swift
+//  Weather App
+//
+//  Created by Yalamandarao Inaganti on 19/12/19.
+//  Copyright © 2019 Yalamandarao Inaganti. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCustomCell: UITableViewCell {
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var countyLabel: UILabel!
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+}
--- Weather App/Weather App/Modules/Home/WeatherHomeViewController.swift
+++ Weather App/Weather App/Modules/Home/WeatherHomeViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class WeatherHomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var weatherTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+}
 
+extension WeatherHomeViewController : UITableViewDelegate, UITableViewDataSource
+{
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+       {
+           return 20
+       }
+       
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           
+           let cell:WeatherCustomCell = tableView.dequeueReusableCell(withIdentifier: "WeatherCustomCellID") as! WeatherCustomCell
+           cell.cityLabel?.text = "Punggol"
+           cell.countyLabel?.text = "Singapore"
+           cell.weatherImageView?.image = UIImage(named: "weather_placeholer")
+           
+           return cell
+        }
+    
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 
+            return 100.0
+        }
 }
 
