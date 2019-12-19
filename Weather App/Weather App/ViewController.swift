881e748760217bd5ae2cff45f2e1600be961d31a
yalamandarao inaganti <yalamandarao.i@gmail.com>
2019-12-19 14:45:50 +0800

    # Created base project.

---
+++ Weather App/Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Yalamandarao Inaganti on 19/12/19.
+//  Copyright © 2019 Yalamandarao Inaganti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
