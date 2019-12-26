07c3bfd90202fb7695d4a52cda552d52929ba0c9
yalamandarao inaganti <yalamandarao.i@gmail.com>
2019-12-26 23:46:27 +0800

    - Added WeatherHomeViewController UI Test cases - Current coverage for UI test cased 70%

---
+++ Weather App/Weather AppUITests/Home/WeatherHomeViewControllerTests.swift
@@ -0,0 +1,62 @@
+//
+//  WeatherHomeViewControllerTests.swift
+//  Weather AppUITests
+//
+//  Created by Singapore on 26/12/19.
+//  Copyright © 2019 Yalamandarao Inaganti. All rights reserved.
+//
+
+import XCTest
+
+class WeatherHomeViewControllerTests: XCTestCase {
+
+    var app: XCUIApplication!
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        
+        app = XCUIApplication()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testInitialStateIsCorrect() {
+        
+        app.launch()
+        
+        let table = XCUIApplication().tables
+        XCTAssertTrue(table.cells.count >= 0)
+       
+    }
+    
+    func testUserSearchString()
+    {
+        app.launch()
+        app.searchFields.element.tap()
+        app.searchFields.element.typeText("Singapore")
+        sleep(3)
+         XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 words matching 'Singapore'")
+    }
+    
+    func testTableViewTap()
+       {
+           app.launch()
+           app.searchFields.element.tap()
+           app.searchFields.element.typeText("Singapore")
+           sleep(3)
+        
+           XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 words matching 'Singapore'")
+        
+           let firstCell = app.tables.cells.element(boundBy: 0)
+           XCTAssertTrue(firstCell.exists)
+           firstCell.tap()
+           
+       }
+
+}
