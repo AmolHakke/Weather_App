56efe031441c171727b04798191dfc2bd4bf21fa
yalamandarao inaganti <yalamandarao.i@gmail.com>
2019-12-27 11:07:12 +0800

    - Added UI Test cases for Home and Detail page

--- Weather App/Weather AppUITests/Home/WeatherHomeViewControllerTests.swift
+++ Weather App/Weather AppUITests/Home/WeatherHomeViewControllerTests.swift
@@ -9,54 +9,67 @@
 import XCTest
 
 class WeatherHomeViewControllerTests: XCTestCase {
-
+    
     var app: XCUIApplication!
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         
         app = XCUIApplication()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testInitialStateIsCorrect() {
-        
+    func testShouldMatchNavigationBarTitle()
+    {
         app.launch()
-        
-        let table = XCUIApplication().tables
-        XCTAssertTrue(table.cells.count >= 0)
-       
+        let nagivationBarTitle = app.navigationBars.element
+        XCTAssertTrue(nagivationBarTitle.staticTexts["Today's Weather"].exists)
     }
     
-    func testUserSearchString()
+    func testShouldMatchSearchBarPlaceholder()
+    {
+        app.launch()
+        let searchBarTitle = app.searchFields.element
+        XCTAssertEqual(searchBarTitle.placeholderValue, "Please enter city / country etc..")
+    }
+    
+    func testInitialStateTableViewCellContainsGreaterThanEqualToZero() {
+        
+        app.launch()
+        let table = XCUIApplication().tables
+        XCTAssertTrue(table.cells.count >= 0)
+        
+    }
+    
+    func testShouldCheckTableViewCellsCountAfterSearchInSearchBar()
     {
         app.launch()
         app.searchFields.element.tap()
         app.searchFields.element.typeText("Singapore")
         sleep(3)
-         XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 words matching 'Singapore'")
+        XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 words matching 'Singapore'")
     }
     
     func testTableViewTap()
-       {
-           app.launch()
-           app.searchFields.element.tap()
-           app.searchFields.element.typeText("Singapore")
-           sleep(3)
+    {
+        app.launch()
+        app.searchFields.element.tap()
+        app.searchFields.element.typeText("Singapore")
+        sleep(3)
         
-           XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 words matching 'Singapore'")
+        XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 words matching 'Singapore'")
         
-           let firstCell = app.tables.cells.element(boundBy: 0)
-           XCTAssertTrue(firstCell.exists)
-           firstCell.tap()
-           
-       }
-
+        let firstCell = app.tables.cells.element(boundBy: 0)
+        XCTAssertTrue(firstCell.exists)
+        firstCell.tap()
+        
+    }
+    
 }
